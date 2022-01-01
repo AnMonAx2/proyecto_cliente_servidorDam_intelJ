bbfb0ec44e42c6a6105c2511b69955ff7c03373d
Ax2 <angelamontoya.ax2@gmail.com>
2022-01-01 19:48:52 +0100

    probando cambios git File

--- Servidor/src/controlador/ProbandoGit.java
+++ Servidor/src/controlador/ProbandoGit.java
@@ -4,7 +4,10 @@ package controlador;/**
 public class ProbandoGit {
     public static void main(String[] args) {
 
-
+        System.out.println("V4 -Git Clonacion y crear tags\n" +
+                "*edita desde GitHub //proceso a la inversa\n" +
+                "*Clonación de repo en local // me lo puedo llevar pc q quiera\n" +
+                "1- edito en .java y lo guarda\uF0E0 abajo  commit changes \n");
 
     }
 }
